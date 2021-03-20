dc0c38387a6fa4720f8acade8736120446d0aef4
Nasium Hill <nhill@ev-c3.kzn.21-school.ru>
2021-03-20 13:54:06 +0300

    echo fixed

--- echo.c
+++ echo.c
@@ -6,17 +6,36 @@
 /*   By: nhill <nhill@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 17:59:28 by nhill             #+#    #+#             */
-/*   Updated: 2021/03/19 17:27:08 by nhill            ###   ########.fr       */
+/*   Updated: 2021/03/20 13:52:24 by nhill            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void		ft_echo(t_parsed_data	*parsed_data)
+int			fn_search(char *s1, char *s2)
 {
-	size_t	index;
+	if (s1 && s2)
+	{
+		if (ft_strncmp(s1, s2, ft_strlen(s1)) == 0)
+			return (1);
+	}
+	return (0);
+}
 
-	printf("%s", parsed_data->argument);
-	if (parsed_data->option == 'n')
+void		fn_echo(char	*parsed_data)
+{
+	int		index;
+	int		flag;
+
+	index = 0;
+	while (parsed_data[index])
+	{
+		if (fn_search(parsed_data[index], "-n"))
+			flag = 1;
+		else
+			printf("%s", parsed_data[index]);
+		index++;
+	}
+	if (flag)
 		printf("\n");
 }
