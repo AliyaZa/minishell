46ed2ef77789259be8183fdfcbb0d8b9a69048f5
Shamil <ishamil86@gmail.com>
2021-03-21 18:16:05 +0300

    refactor main - cycle to main_cycle.c file

--- main.c
+++ main.c
@@ -16,23 +16,7 @@
 // parser allocate memory, free it after usage
 int		main(int argc, char **argv, char **env)
 {
-	t_parsed_data	*parsed_data;
-	t_env			*env_data;
-
-	env_data = parse_env(env);
-	parsed_data = parser();
-	if (fn_search(parsed_data->command, "echo"))
-	{
-		fn_echo(parsed_data->rest_string);
-	}
-	else if (fn_search(parsed_data->command, "pwd"))
-	{
-		printf("%s\n", take_value_by_key(env_data, "PWD"));
-	}
-	else if (fn_search(parsed_data->command, "env"))
-	{
-		print_env(env_data);
-	}
+	main_cycle(env);
 	//print_env(env_data);
 	printf("first argument - %s\nargc - %d\n", argv[0], argc);
 	return (0);
