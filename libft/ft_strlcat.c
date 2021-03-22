c065f06412a20768786b4af49f24957f13851eec
Nasium Hill <nhill@et-h4.kzn.21-school.ru>
2021-03-22 16:49:40 +0300

    libft changes

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nhill <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: nhill <nhill@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 19:28:58 by nhill             #+#    #+#             */
-/*   Updated: 2020/11/10 15:15:23 by nhill            ###   ########.fr       */
+/*   Updated: 2021/03/22 16:12:42 by nhill            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+char	*ft_strlcat(char *dst, const char *src)
 {
 	size_t	i;
 	size_t	lens;
@@ -23,11 +23,8 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	while (dst[i])
 		i++;
 	lend = i;
-	if (dstsize > i)
-		while (src[lens] && dstsize - i > 1)
-			dst[i++] = src[lens++];
-	dst[i] = '\0';
 	while (src[lens])
-		lens++;
-	return ((lend < dstsize ? lend : dstsize) + lens);
+		dst[i++] = src[lens++];
+	dst[i] = '\0';
+	return (dst);
 }
--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -3,32 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nhill <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: nhill <nhill@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/29 14:15:52 by nhill             #+#    #+#             */
-/*   Updated: 2020/11/06 19:05:08 by nhill            ###   ########.fr       */
+/*   Updated: 2021/03/22 16:08:52 by nhill            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+char	*ft_strlcpy(char *dst, const char *src)
 {
 	size_t	i;
 
 	if (dst == NULL || src == NULL)
 		return (0);
-	if (dstsize == 0)
-		return (ft_strlen(src));
 	i = 0;
-	if (dstsize > 0)
+	while (src[i] != 0)
 	{
-		while (i < dstsize - 1 && src[i] != 0)
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
+		dst[i] = src[i];
+		i++;
 	}
-	return (ft_strlen(src));
+	dst[i] = '\0';
+	return (dst);
 }
