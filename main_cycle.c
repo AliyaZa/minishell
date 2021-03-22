7f79d24f25029e7ec1965b19b012bb8f10e3b29d
Shamil <ishamil86@gmail.com>
2021-03-22 09:21:20 +0300

    cycling program

--- main_cycle.c
+++ main_cycle.c
@@ -5,18 +5,21 @@ void	main_cycle(char **env)
 	t_parsed_data	*parsed_data;
 	t_env			*env_data;
 
-	env_data = parse_env(env);
-	parsed_data = parser();
-	if (fn_search(parsed_data->command, "echo"))
+	while (1)
 	{
-		fn_echo(parsed_data->rest_string);
-	}
-	else if (fn_search(parsed_data->command, "pwd"))
-	{
-		printf("%s\n", take_value_by_key(env_data, "PWD"));
-	}
-	else if (fn_search(parsed_data->command, "env"))
-	{
-		print_env(env_data);
+		env_data = parse_env(env);
+		parsed_data = parser();
+		if (fn_search(parsed_data->command, "echo"))
+		{
+			fn_echo(parsed_data->rest_string);
+		}
+		else if (fn_search(parsed_data->command, "pwd"))
+		{
+			printf("%s\n", take_value_by_key(env_data, "PWD"));
+		}
+		else if (fn_search(parsed_data->command, "env"))
+		{
+			print_env(env_data);
+		}
 	}
 }
