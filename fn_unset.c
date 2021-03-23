8c941e3607758442d4f92e5862ce8afc71c4311a
Nasium Hill <nhill@si-j2.kzn.21-school.ru>
2021-03-23 18:43:02 +0300

    func

---
+++ fn_unset.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fn_unset.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhill <nhill@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/23 16:38:20 by nhill             #+#    #+#             */
+/*   Updated: 2021/03/23 17:00:15 by nhill            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell2/minishell.h"
+
+void		fn_del_el(t_env **root)
+{
+	if((*root)->key)
+	{
+		free((*root)->key);
+		(*root)->key = NULL;
+	}
+	if((*root)->value)
+	{
+		free((*root)->value);
+		(*root)->value = NULL;
+	}
+}
+
+void		fn_unset(t_env **our_env, char *user_commands)
+{
+	t_env	env;
+	t_env	*prev;
+	t_env	*root;
+
+	//env = 0;
+	root = *our_env;
+	if (fn_search((*our_env)->key, user_commands))
+		*our_env = (*our_env)->next;
+	else
+	{
+		while (root->next)
+		{
+			prev = root;
+			root = root->next;
+			if (fn_search(root->key, user_commands))
+			{
+				prev->next = root->next;
+				fn_del_el(&root);
+				if (root)
+					free(root);
+				root = NULL;
+				break;
+			}
+		}
+	}
+	root = *our_env;
+}
