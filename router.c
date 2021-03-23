44be4ede69262654bb1a9806f4aecda12f9bea02
Nasium Hill <nhill@si-j2.kzn.21-school.ru>
2021-03-23 18:52:36 +0300

    func

--- router.c
+++ router.c
@@ -6,7 +6,7 @@
 /*   By: nhill <nhill@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 14:44:46 by mismene           #+#    #+#             */
-/*   Updated: 2021/03/23 18:44:20 by nhill            ###   ########.fr       */
+/*   Updated: 2021/03/23 18:47:25 by nhill            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	router(t_parsed_data *parsed_data, t_env *env_data)
 {
 	if (fn_search("echo", parsed_data->command))
 	{
-//		printf("command = %s\necho = %s\n",parsed_data->command, parsed_data->rest_string);
+		printf("command = %s\necho = %s\n",parsed_data->command, parsed_data->rest_string);
 		fn_echo(parsed_data->rest_string);
 	}
 	else if (fn_search(parsed_data->command, "pwd") && *parsed_data->command)
