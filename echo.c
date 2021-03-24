691678f0c86b3c28c9c5aff4c3800b4fdd02c24a
Nasium Hill <nhill@ev-b4.kzn.21-school.ru>
2021-03-24 16:24:37 +0300

    fix echo

--- echo.c
+++ echo.c
@@ -6,26 +6,25 @@
 /*   By: nhill <nhill@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 17:59:28 by nhill             #+#    #+#             */
-/*   Updated: 2021/03/24 14:51:46 by mismene          ###   ########.fr       */
+/*   Updated: 2021/03/24 16:24:21 by nhill            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void        fn_echo(char    *output)
+void	fn_echo(char *output)
 {
-    int        flag;
+	int		flag;
 
-    flag = 0;
-   // parsed_data++;
-    if (fn_search(" -n" ,output))
-    {
-        output += 4;
-        flag = 1;
-    }
+	flag = 0;
+	if (fn_search(" -n" ,output))
+	{
+		output += 4;
+		flag = 1;
+	}
 	else
 		output++;
-    write(1, output, ft_strlen(output));
-    if (flag)
-        write(1, "\n", 1);
+	write(1, output, ft_strlen(output));
+	if (!flag)
+		write(1, "\n", 1);
 }
