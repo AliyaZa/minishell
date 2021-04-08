eab40adcbef8ff172bd89f1d3af9f666e200dac6
Nasium Hill <nhill@ev-s3.kzn.21-school.ru>
2021-04-08 16:47:12 +0300

    new

--- build_in/fn_cd.c
+++ build_in/fn_cd.c
@@ -6,7 +6,7 @@
 /*   By: nhill <nhill@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/20 14:35:24 by nhill             #+#    #+#             */
-/*   Updated: 2021/04/08 16:15:48 by nhill            ###   ########.fr       */
+/*   Updated: 2021/04/08 16:40:46 by nhill            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,11 +70,11 @@ static char	*fn_get_path(t_parsed_data *parsed_data, t_command *command)
 	home = NULL;
 	tmp = NULL;
 	if ((!(home = fn_get_el(parsed_data, "HOME")) && !(command->command)) ||
-		fn_search(command->rest_string, "~"))
+		fn_search(command->argument, "~"))
 		return (NULL);
-	else if (command->rest_string && command->rest_string[0] == '/')
-		new_path = ft_strdup(command->rest_string);
-	else if (fn_search(command->rest_string, "-"))
+	else if (command->argument && command->argument[0] == '/')
+		new_path = ft_strdup(command->argument);
+	else if (fn_search(command->argument, "-"))
 	{
 		if (!fn_get_el(parsed_data->env_data->key, "OLDPWD"))
 			return (NULL);
