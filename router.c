d7ed57c9ce60d2a16b0fd3c42df3187ae4db704a
Nasium Hill <nhill@ev-r4.kzn.21-school.ru>
2021-04-09 18:19:28 +0300

    exit

--- router.c
+++ router.c
@@ -6,7 +6,7 @@
 /*   By: nhill <nhill@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 14:44:46 by mismene           #+#    #+#             */
-/*   Updated: 2021/04/09 17:32:36 by nhill            ###   ########.fr       */
+/*   Updated: 2021/04/09 18:18:50 by nhill            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,10 @@ void	router(t_parsed_data *parsed_data, t_command *command)
 	else if (fn_search(command->command, "unset") && *command->command)
 	{
 		fn_unset(parsed_data, command);
+	}
+	else if (fn_search(command->command, "exit") && *command->command)
+	{
+		exit(0);
 	}
 	else
 	{
