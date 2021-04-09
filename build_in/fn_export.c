334c59265ee9100cd65039299c6be37f4acd4537
Nasium Hill <nhill@ev-r4.kzn.21-school.ru>
2021-04-09 17:18:58 +0300

    fix

--- build_in/fn_export.c
+++ build_in/fn_export.c
@@ -6,24 +6,26 @@
 /*   By: nhill <nhill@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 16:07:01 by nhill             #+#    #+#             */
-/*   Updated: 2021/04/08 20:38:36 by nhill            ###   ########.fr       */
+/*   Updated: 2021/04/09 17:18:38 by nhill            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static void	print_export(t_parsed_data *parsed_data)
+static void	print_export(t_env *parsed_data)
 {
 	int		i;
+	t_env *parsed_data1;
 
 	i = 0;
-	while (parsed_data->env_data != NULL)
+	parsed_data1 = parsed_data;
+	while (parsed_data1 != NULL)
 	{
-		if (!parsed_data->env_data->equal)
-			printf("declare -x %s=\"%s\"\n", parsed_data->env_data->key, parsed_data->env_data->value);
+		if (!parsed_data1->equal)
+			printf("declare -x %s=\"%s\"\n", parsed_data1->key, parsed_data1->value);
 		else
-			printf("declare -x %s\n", parsed_data->env_data->key);
-		parsed_data->env_data = parsed_data->env_data->next;
+			printf("declare -x %s\n", parsed_data1->key);
+		parsed_data1 = parsed_data1->next;
 	}
 }
 
@@ -44,6 +46,6 @@ void	fn_export(t_parsed_data *parsed_data, t_command *command)
 	}
 	else
 	{
-		print_export(parsed_data);
+		print_export(parsed_data->env_data);
 	}
 }
