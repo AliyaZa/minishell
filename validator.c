55179ff54720e91be841ee79e1ee3dbc8a3ab9c7
Mussels Ismene <ishamil86@gmail.com>
2021-04-11 12:29:54 +0300

    validating quotes

--- validator.c
+++ validator.c
@@ -12,19 +12,31 @@
 
 #include "minishell.h"
 
-void	quotes_stack(char *string)
+void	quotes_stack(char *string, size_t quotes_count)
 {	
 	char	main_quotes;
+	char	*stack;
+	int		index;
 
+	index = 0;
+	stack = malloc(sizeof(char) * quotes_count + 5);
+	stack[quotes_count] = 0;
+	if (!stack)
+	{
+		ft_putstr_fd("memory not allocated", 1);
+		return ;
+	}
 	main_quotes = 0;
 	while (*string)
 	{
 		if (*string == '"' || *string == '\'')
 		{
+			stack[index++] = *string;
 			main_quotes = *string;
 		}
 		string++;
 	}
+	printf("%s\n", stack);
 }
 
 void	validate_quotes(char *string)
@@ -36,5 +48,5 @@ void	validate_quotes(char *string)
 	quotes = count_req_chars(string, '\'');
 	if ((quotes % 2) || (d_quotes % 2))
 		ft_putstr_fd("minishell: syntax error: unexpected end of file", 1);
-	quotes_stack(string);
+	quotes_stack(string, d_quotes + quotes);
 }
