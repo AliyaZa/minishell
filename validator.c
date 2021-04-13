7868930b670be4a1900be1fb24a95cd879475e50
Mussels Ismene <ishamil86@gmail.com>
2021-04-13 10:26:21 +0300

    I am doing new validator

--- validator.c
+++ validator.c
@@ -12,81 +12,36 @@
 
 #include "minishell.h"
 
-char	*get_key(char *string)
-{
-	int		index;
-	char	*key;
-
-	index = 0;
-	key = ft_strdup(string);
-	while (key[index])
-	{
-		if (key[index] == ' ' || key[index] == '"')
-		{
-			key[index] = 0;
-			return (key);
-		}
-		index++;
-	}
-	return (NULL);
-}
-
 void	validator(char **string, t_env *env, t_command *command)
 {
-	char	*start;
+	char	*tmp;
+	char	*tmp1;
+	char	*p;
 	int		index;
-	char	*save;
 	char	flag;
-	int		k;
-	char	*value;
-	char	*key;
 
-	key = NULL;
 	index = 0;
-	start = *string;
-	save = NULL;
+	p = *string;
+	tmp1 = NULL;
+	(void)(env->key);
+	(void)(command->command);
 	flag = 0;
-	k = 0;
-	(void)env->key;
-	while (*start)
+	while (p[index])
 	{
-		if (*start == '"' || *start == '\'')
+		if (p[index] == '"' || p[index] == '\'')
 		{
-			if (!flag)
-				flag = *start;
-			else
+			if (!flag || p[index] == flag)
 			{
-				if (flag == *start)
-				{
+				if (!flag)
+					flag = p[index];
+				else if (p[index] == flag)
 					flag = 0;
-				}
+				tmp = ft_substr(p, 0, index);
+				tmp1 = ft_substr(p, index + 1, ft_strlen(p));
+				p = ft_strjoin(tmp, tmp1);
+				continue ;
 			}
-			*start = 0;
-			save = ft_strdup(start + 1);
-			*string = ft_strjoin(*string, save);
-			free_str(&save);
-			start = &start[index];
 		}
-		if (*start == '$' && (flag == 0 || flag == '"'))
-		{
-			*start++ = 0;
-			key = get_key(start);
-			value = get_value_by_key(env, key);
-			save = ft_strdup(start + ft_strlen_c(start, ' '));
-			*string = ft_strjoin(*string, value);
-			*string = ft_strjoin(*string, save);
-			free_str(&save);
-			start = *string;
-			start = &start[index + ft_strlen_c(start, ' ')];
-		}
-		else
-		{
-			start++;
-			index++;
-		}
-	}
-	if (flag)
-	{
-		fn_errors(command, SYNTAX_ERROR);
+		index++;
 	}
 }
