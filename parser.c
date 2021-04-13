a116fedefe2c4307ebbe2a92320c1e932fda9fa1
Mussels Ismene <ishamil86@gmail.com>
2021-04-13 19:04:46 +0300

    testing redirects

--- parser.c
+++ parser.c
@@ -102,16 +102,15 @@ void	parser(t_command **command, t_env *env)
 {
 	if (!ft_strlen((*command)->raw_string) && (*command)->queue)
 	{
-		// printf("queue dup of %s\n", (*command)->queue);
 		(*command)->raw_string = ft_strdup((*command)->queue);
 	}
 	if (!ft_strncmp((*command)->raw_string, "\n", 1))
 		return ;
 	replace_symbol(&(*command)->raw_string, '\n', '\0');
 	semicolon(command);
-	// printf("raw_string: %s\nqueue: %s\n", (*command)->raw_string, (*command)->queue);
 	(*command)->rest_string = determine_command(command);
 	determine_options(command);
 	(*command)->argument = determine_argument(*command);
 	validator(&(*command)->argument, env, *command);
+	(*command)->splited = ft_split((*command)->raw_string, ' ');
 }
