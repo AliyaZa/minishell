e7be06d9060a661859b2c2480ca842ab4085b90b
Nasium Hill <nhill@si-n4.kzn.21-school.ru>
2021-04-27 20:35:43 +0300

    shlvl

--- build_in/fn_fork.c
+++ build_in/fn_fork.c
@@ -6,7 +6,7 @@
 /*   By: nhill <nhill@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 18:57:20 by nhill             #+#    #+#             */
-/*   Updated: 2021/04/26 20:40:25 by nhill            ###   ########.fr       */
+/*   Updated: 2021/04/27 20:34:05 by nhill            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,16 +45,33 @@ static char	**fn_arr(t_env *env)
 	return (res);
 }
 
+static int	check_digit(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 static int	fn_path(t_parsed_data *parsed_data, t_command *command)
 {
 	t_env	*path;
 	char	**places;
 	int		kol;
 	char	*path_to;
+	t_env	*mini;
+	int		level;
 
 	kol = 0;
 	places = NULL;
 	path_to = NULL;
+	//if (command->flags->is_bin != 1)
 	path = fn_get_el(parsed_data, "PATH");
 	if (path)
 		places = ft_split(path->value, ':');
@@ -71,6 +88,21 @@ static int	fn_path(t_parsed_data *parsed_data, t_command *command)
 		path_to = ft_strdup(command->argument);
 	if (command->fd[1] > 1)
 		dup2(command->fd[1], 1);
+	if (fn_search(command->command, "minishell"))
+	{
+		mini = fn_get_el(parsed_data, "SHLVL");
+		if (check_digit(mini->value) == 0)
+			level = 0;
+		else
+			level = ft_atoi(mini->value);
+		if ((level < 999))
+		{
+			if (!(mini->value = ft_itoa(level + 1)))
+				return(errno);
+		}
+		else if (level == 999)
+			mini->value = "";
+	}
 	if ((execve(path_to, command->splited, fn_arr(parsed_data->env_data)) == 0))
 	{
 		dup2(1, command->fd[1]);
