e65d2fbfe1ef51bd6caf857044f45bacd5b48e6d
Nasium Hill <nhill@ev-s1.kzn.21-school.ru>
2021-05-03 20:15:41 +0300

    pipes algorithm

--- build_in/fn_pipe.c
+++ build_in/fn_pipe.c
@@ -6,103 +6,93 @@
 /*   By: nhill <nhill@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/02 18:10:44 by nhill             #+#    #+#             */
-/*   Updated: 2021/05/03 19:04:58 by nhill            ###   ########.fr       */
+/*   Updated: 2021/05/03 20:15:13 by nhill            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-// int		kol_pipe(char **arr)
-// {
-// 	int		i;
-// 	int		j;
-// 	i = 0;
-// 	j = 0;
-// 	while (arr[i] != NULL)
-// 	{
-// 		while (arr[i][j])
-// 		{
-// 			j++;
-// 		}
-// 		i++;
-// 	}
-// 	return(i);
-// 	//sizeof(arr)/sizeof(arr[0]);
-// }
-//форк перепиши
+static void	ft_success_fork(t_parsed_data *parsed_data, t_command *command,
+	int i, int fd[command->pipes_quantity][2], char *path)
+{
+	int j;
 
-// static void	ft_success_fork(t_parsed_data *parsed_data, t_command *command,
-// 	int i, int **fd, char *path)
-// {
-// 	int j;
+	if (i == 0)
+	{
+		i < (int)command->pipes_quantity ? dup2(fd[i][1], 1) : 0;
+//		ft_close_descript(all->count_com, fd, -1);
+		j = -1;
+		while (++j < (sizeof(command->pipes)/sizeof(command->pipes[0])) - 1)
+		{
+			close(fd[j][0]);
+			close(fd[j][1]);
+		}
+	}
+	else if (i < ((int)command->pipes_quantity - 1))
+	{
+		dup2(fd[i - 1][0], 0);
+		dup2(fd[i][1], 1);
+//		ft_close_descript(all->count_com, fd, -1);
+		j = -1;
+		while (++j < (int)command->pipes_quantity - 1)
+		{
+			close(fd[j][0]);
+			close(fd[j][1]);
+		}
+	}
+	else
+	{
+		dup2(fd[i - 1][0], 0);
+//		ft_close_descript(all->count_com, fd, -1);
+		j = -1;
+		while (++j < (sizeof(command->pipes)/sizeof(command->pipes[0])) - 1)
+		{
+			close(fd[j][0]);
+			close(fd[j][1]);
+		}
+	}
+	command->fd[1] != 1 ? dup2(command->fd[1], fd[i][0]) : 0;
+	command->fd[0] != 1 ? dup2(command->fd[0] , 1) : 0;
+	execve(path, command->splitted, fn_arr(parsed_data->env_data));
+}
 
-// 	if (i == 0)
-// 	{
-// 		i < (sizeof(command->pipes)/sizeof(command->pipes[0])) ? dup2(fd[i][1], 1) : 0;
-// //		ft_close_descript(all->count_com, fd, -1);
-// 		j = -1;
-// 		while (++j < (sizeof(command->pipes)/sizeof(command->pipes[0])) - 1)
-// 		{
-// 			close(fd[i][0]);
-// 			close(fd[i][1]);
-// 		}
-// 	}
-// 	else if (i < ((sizeof(command->pipes)/sizeof(command->pipes[0])) - 1))
-// 	{
-// 		dup2(fd[i - 1][0], 0);
-// 		dup2(fd[i][1], 1);
-// //		ft_close_descript(all->count_com, fd, -1);
-// 		j = -1;
-// 		while (++j < (sizeof(command->pipes)/sizeof(command->pipes[0])) - 1)
-// 		{
-// 			close(fd[i][0]);
-// 			close(fd[i][1]);
-// 		}
-// 	}
-// 	else
-// 	{
-// 		dup2(fd[i - 1][0], 0);
-// //		ft_close_descript(all->count_com, fd, -1);
-// 		j = -1;
-// 		while (++j < (sizeof(command->pipes)/sizeof(command->pipes[0])) - 1)
-// 		{
-// 			close(fd[i][0]);
-// 			close(fd[i][1]);
-// 		}
-// 	}
-// 	command->fd[1] != 1 ? dup2(command->fd[1], fd[i][0]) : 0;
-// 	command->fd[0] != 1 ? dup2(command->fd[0] , 1) : 0;
-// 	execve(path, command->splitted, fn_arr(parsed_data->env_data));
-// }
+int		fn_make_pipe(t_parsed_data *parsed_data ,t_command *command)
+{
+	int		i;
+	int		fd[command->pipes_quantity][2];
+	char	*path;
+	int		error;
 
-// // kol_pipe(command->pipes);
-// int		fn_pipe(t_parsed_data *parsed_data ,t_command *command, int kol)
-// {
-// 	int		i;
-// 	int		fd[kol][2];
-// 	char	*path;
+	i = 0;
+	while (i++ < (int)command->pipes_quantity)
+		pipe(fd[i]);
+	i = 0;
+	while (i < (int)command->pipes_quantity)
+	{
+		if ((path = fn_path(parsed_data ,command, &error)) != NULL)
+		{
+			fork() == 0 ? ft_success_fork(parsed_data, command,i, fd, path) : 0;
+			free (path);
+			path = NULL;
+		}
+		i++;
+	}
+	int j = -1;
+	while (++j < (int)command->pipes_quantity - 1)
+	{
+		close(fd[i][0]);
+		close(fd[i][1]);
+	}
+	return (error);
+}
 
-// 	i = 0;
-// 	while (i++ < kol)
-// 		pipe(fd[i]);
-// 	i = 0;
-// 	while (i < kol)
-// 	{
-// 		if (path = fn_path(parsed_data ,command))
-// 		{
-// 			fork() == 0 ? ft_success_fork(parsed_data, command,
-// 			i, fd, path) : 0;
-// 			free (path);
-// 			path = NULL;
-// 		}
-// 		i++;
-// //		router(parsed_data, command);
-// 	}
-// 	int j = -1;
-// 	while (++j < kol - 1)
-// 	{
-// 		close(fd[i][0]);
-// 		close(fd[i][1]);
-// 	}
-// 	return (ft_wait(i));
-// }
+int		fn_pipe(t_parsed_data *parsed_data ,t_command *command)
+{
+	int		error;
+
+	error = 0;
+	error = fn_make_pipe(parsed_data, command);
+	if (error != 0)
+		fn_errors(command, error);
+	exit (0);
+}
