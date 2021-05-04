f5bf1384002a32432de8533d9c8cfd28e09ad080
Mussels Ismene <ishamil86@gmail.com>
2021-05-04 12:10:58 +0300

    termcap minor fix

--- termcap.c
+++ termcap.c
@@ -20,12 +20,17 @@ void	ctr_d(void)
 
 static void	new_symbol(char **str, t_command *command, int *current, size_t *c_p)
 {
-	ft_putstr_fd(*str, 1);
-	if (!command->raw_string)
-		command->raw_string = ft_strnew(0);
-	command->raw_string = ft_strjoin_free(command->raw_string, *str, 1);
-	*current = -1;
-	*c_p += ft_strlen(*str);
+	if (!ft_strncmp(*str, "\x7f", ft_strlen("\x7f")) && *c_p < 2)
+		;
+	else
+	{
+		ft_putstr_fd(*str, 1);
+		if (!command->raw_string)
+			command->raw_string = ft_strnew(0);
+		command->raw_string = ft_strjoin_free(command->raw_string, *str, 1);
+		*current = -1;
+		*c_p += ft_strlen(*str);
+	}
 }
 
 void		fn_termcap(t_command *command, char **history)
@@ -38,12 +43,10 @@ void		fn_termcap(t_command *command, char **history)
 	current = -1;
 	str = ft_calloc(2000, sizeof(char));
 	str[0] = 0;
-	cursor_position = 0;
+	cursor_position = 1;
 	while (str[0] != '\n')
 	{
 		l = read(0, str, 1998);
-		// if (ft_strncmp_end(str, "\x03", 1) && ft_strncmp_end(str, "\x7f", 1))
-		// 	cursor_position += l;
 		str[l] = 0;
 		if (!ft_strncmp(str, "\x04", 1) && cursor_position == 1)
 			ctr_d();
@@ -61,10 +64,10 @@ void		fn_termcap(t_command *command, char **history)
 		}
 		else if (!ft_strncmp(str, "\e[D", 3) || !ft_strncmp(str,
 		"\e[C", 3) || !ft_strncmp(str, "\t", 1) || !ft_strncmp(str, "\034", 2)
-		|| !ft_strncmp(str, "\v", 1))
+		|| !ft_strncmp(str, "\v", 1) || !ft_strncmp(str, "\b", 1))
 			;
 		else if (((!ft_strncmp_end(str, "\177", 1) && !ft_strncmp(str, "\x7f",
-		ft_strlen("\x7f"))) || !ft_strncmp(str, "\177", 1)) && (cursor_position > 0))
+		ft_strlen("\x7f"))) || !ft_strncmp(str, "\177", 1)) && (cursor_position > 1))
 			backspace(&command->raw_string, &cursor_position);
 		else
 			new_symbol(&str, command, &current, &cursor_position);
