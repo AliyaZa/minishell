b446969d53da9887ec707e8fbfa49c08289b0edb
Mussels Ismene <ishamil86@gmail.com>
2021-05-05 17:29:03 +0300

    hope it's working

--- parser/parser.c
+++ parser/parser.c
@@ -25,20 +25,87 @@ static void	determine_command_struct(t_command *command, t_env *env)
 		command->raw_string = validate_raw_string(command->raw_string);
 }
 
+void		validate_q(char *quote, char *p, size_t index)
+{
+	if (*quote == 0)
+		*quote = p[index];
+	else if (*quote == p[index])
+		*quote = 0;
+}
+
+void		validate_pipe(char **raw_string)
+{
+	size_t	index;
+	char	quote;
+	char	*p;
+
+	p = *raw_string;
+	index = 0;
+	quote = '\0';
+	while (p[index])
+	{
+		if (p[index] == '\'' || p[index] == '"')
+			validate_q(&quote, p, index);
+		else if (p[index] == '|' && quote)
+			p[index] = -1;
+		else if (p[index] == '\\')
+			p[index + 1] = -1;
+		index++;
+	}
+}
+
+void		validate_spaces(char **string)
+{
+	char	*p;
+	size_t	index;
+	char	quote;
+
+	quote = '\0';
+	p = *string;
+	index = 0;
+	while (p[index])
+	{
+		if (p[index] == '\'' || p[index] == '"')
+			validate_q(&quote, p, quote);
+		else if (p[index] == ' ' && quote)
+			p[index] = -1;
+	}
+}
+
+void		replace_symbol3(char ****array, char old, char new)
+{
+	char	***p;
+	size_t	index;
+
+	p = *array;
+	index = 0;
+	while (p[index])
+	{
+		replace_symbol_array(p, old, new);
+		index++;
+	}
+}
+
 char		***get_pipes(t_command *command)
 {
 	char	**matrix;
 	char	***pipes;
 	size_t	i;
+	char	*p;
 
 	i = 0;
+	p = ft_strdup(command->raw_string);
 	if (ft_strchr(command->raw_string, '|'))
 		command->flags->pipe = 1;
-	matrix = ft_split(command->raw_string, '|');
+	validate_pipe(&p);
+	matrix = ft_split(p, '|');
+	replace_symbol_array(&matrix, -1, '|');
 	pipes = (char ***)malloc(sizeof(char **) * array_size(matrix));
 	while (matrix[i])
 	{
+		validate_spaces(&matrix[i]);
 		pipes[i] = ft_split(matrix[i], ' ');
+		replace_symbol3(&pipes, -1, ' ');
 		i++;
 	}
 	return (pipes);
@@ -57,7 +124,7 @@ void		parser(t_command *command, t_env *env)
 	determine_command_struct(command, env);
 	command->splitted = get_splitted(command->raw_string);
 	command->pipes = get_pipes(command);
-	print_array3(command->pipes);
+	// print_array3(command->pipes);
 	// command->pipes_quantity = array_size(command->pipes);
 	replace_symbol_array(&command->splitted, -1, ' ');
 	delete_quotes(&command->splitted);
